d8f21c40a02f6f6761834cc115c95c2ed15fe877
Marcos Chanca <daxter2424@hotmail.com>
2022-07-08 10:31:55 +0200

    Fin sección 4

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,34 +1,30 @@
 import { NgModule } from "@angular/core";
-import { Router, RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
+
+import { PagesRoutingModule } from "./pages/pages.routing";
+
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { Grafica1Component } from "./pages/grafica1/grafica1.component";
-import { NopagefoundComponent } from "./pages/nopagefound/nopagefound.component";
-import { PagesComponent } from "./pages/pages.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
+import { NopagefoundComponent } from "./nopagefound/nopagefound.component";
+import { AuthRoutingModule } from "./auth/auth.routing";
+
 
 const routes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'grafica1', component: Grafica1Component },
-            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-        ]
-    },
+
+    // path: '/dashboard' --> PagesRouting
+    // path: '/login' --> AuthRouting
     
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    
-    { path: '**', component: NopagefoundComponent }
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: '**', component: NopagefoundComponent },
 ]
 
 @NgModule({
     declarations: [],
-    imports: [ RouterModule.forRoot( routes ) ],
+    imports: [ 
+        RouterModule.forRoot( routes ),
+        PagesRoutingModule,
+        AuthRoutingModule
+    ],
     exports: [RouterModule]
 })
 
